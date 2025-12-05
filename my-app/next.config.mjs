a2c7b4784a1cc89a86c4dd5c34e5d2397c737f32
Md Aman <aman003245@gmail.com>
2025-12-06 03:44:56 +0530

    do

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,11 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     eslint: {
-//         ignoreDuringBuilds: true,
-//     },
-// };
 
-// export default nextConfig;
+
+
 
 
 /** @type {import('next').NextConfig} */
