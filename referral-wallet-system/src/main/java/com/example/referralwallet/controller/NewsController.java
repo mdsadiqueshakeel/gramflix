f92cf7484a0fcb6e99e2cb761a47a2b213c8be6a
mdsadiqueshakeel <mdsadiqueshakeel@gmail.com>
2025-12-06 13:31:36 +0530

    ci/cd check 1

--- referral-wallet-system/src/main/java/com/example/referralwallet/controller/NewsController.java
+++ referral-wallet-system/src/main/java/com/example/referralwallet/controller/NewsController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/news")
+@RequestMapping("/api/newsu")
 @RequiredArgsConstructor
 @Slf4j
 public class NewsController {
