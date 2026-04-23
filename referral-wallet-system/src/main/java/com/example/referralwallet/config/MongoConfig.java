62f7d7d9ec155a553c4a71b2b31c3188c22f3691
nexromedia <nexromedia@gmail.com>
2026-04-23 15:31:18 +0530

    package updated

--- referral-wallet-system/src/main/java/com/example/referralwallet/config/MongoConfig.java
+++ referral-wallet-system/src/main/java/com/example/referralwallet/config/MongoConfig.java
@@ -1,18 +1,22 @@
 package com.example.referralwallet.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 public class MongoConfig {
     @Value("${spring.data.mongodb.uri}")
     private String mongoUri;
+
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
 
     @Bean
     public MongoClient mongoClient() {
@@ -25,6 +29,6 @@ public class MongoConfig {
 
     @Bean
     public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(mongoClient(), new ConnectionString(mongoUri).getDatabase());
+        return new MongoTemplate(mongoClient(), databaseName);
     }
 }
